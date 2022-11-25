47e67068b6a5aef410fd736ff89ce25dec802750
karimandrusevych <karimandrusevych@gmail.com>
2022-11-26 01:10:25 +0700

    init

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,21 @@
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    expect(true).to.equal(true)
+  })
+})
+ 
+describe('My second test', function() {
+  it('Gets, types and asserts', function() {
+    cy.visit('https://example.cypress.io')
+ 
+    cy.contains('type').click()
+ 
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+ 
+    // Get an input, type into it and verify that the value has been updated
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
